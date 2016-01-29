24951b593f992d8ca8e2c6157a6eb75d44c103fc
xylophone234 <595403646@qq.com>
2016-01-29 10:25:21 +0800

    est theta then est a,b

--- irt.py
+++ irt.py
@@ -2,9 +2,9 @@
 import numpy as np
 import math
 D=1.7
-# 500students 30 items
-n=100
-m=1 
+# n student m item
+n=1000
+m=1000 
 theta=np.random.normal(0,1,n)
 a=np.random.lognormal(0,0.3,m)
 b=np.random.normal(0,1,m)
@@ -113,7 +113,7 @@ def est2(n):
 	return lh,errora,errorb
 
 def abs(x):
-	return x if x<=0 else -x
+	return x if x>=0 else -x
 
 def likehood(theta,u,a,b,c):
 	global D
@@ -168,31 +168,37 @@ def is_negtive_define(ha,hab,hb):
 
 def estab(theta,u,k):
 	global c
+	num_step=0
 	eps=1e-6
 	li=[]
+	if u.sum()==u.shape[0]:return 0.0,0.0,-3.00
+	if u.sum()==0:return 0.0,0.0,3.00
 	for i in range(k):
 		ha,hab,hb=1,0,1
+		num_init=0
 		# Hession Matrix negtive define for a0,b0
-		while not is_negtive_define(ha,hab,hb):
+		while (not is_negtive_define(ha,hab,hb)) and num_init<100:
+			num_init+=1
 			ea=2*np.random.random_sample()
 			eb=6*np.random.random_sample()-3
-			lh=likehood(theta,u,ea,eb,c)
+			# lh=likehood(theta,u,ea,eb,c)
 			
 			xa,xb=getDerivative(theta,u,ea,eb,c)
 			ha,hab,hb=getHess(theta,u,ea,eb,c)
 
 			det=ha*hb-hab**2
 			hi=1/det
-		
+		lh=likehood(theta,u,ea,eb,c)
 		deltaxa=hi*(hb*xa-hab*xb)
 		deltaxb=hi*(-hab*xa+ha*xb)
-		while abs(xa)>=eps and abs(xb)>=eps:
+		while abs(xa)>=eps and abs(xb)>=eps and num_step<100:
+			num_step+=1
 			miu=1.0
 			tempa=ea-miu*deltaxa
 			tempb=eb-miu*deltaxb
 			ha,hab,hb=getHess(theta,u,tempa,tempb,c)
 			# newtown down hill method 
-			while (not is_negtive_define(ha,hab,hb)) or lh>likehood(theta,u,tempa,tempb,c):
+			while tempa<0 or tempa>2 or tempb<-3 or tempb>3 or (not is_negtive_define(ha,hab,hb)) or lh>likehood(theta,u,tempa,tempb,c):
 				miu*=0.5
 				tempa=ea-miu*deltaxa
 				tempb=eb-miu*deltaxb
@@ -240,6 +246,192 @@ def testall(num):
 	rs=np.array(rs).reshape(-1,2)
 	print ((ts-rs)**2).sum(axis=0)/num
 	return ts,rs
+
+# init theta0 is fixed
+def esttheta(a,b,c,u):
+	global D
+	# print u
+	num_step=0
+	if u.sum()==u.shape[0]:return 3.00
+	if u.sum()==0:return -3.00
+	etheta=math.log(float(u.sum())/(u.shape[0]-u.sum()))
+	dx=1.00
+	eps=1e-6
+	# print 0,etheta,abs(dx),abs(dx)>eps
+	while abs(dx)>eps and num_step<30:
+		# print 'in while'
+		num_step+=1
+		tb=etheta-b
+		p=c+(1-c)/(1+math.e**(-D*a*tb))
+		d=D*a*(u-p)*(p-c)/(1-c)/p
+		dd=D**2*a**2*(p-c)*(u*c-p**2)*(1-p)/(p**2*(1-c)**2)
+
+		dx=d.sum()
+		ddxx=dd.sum()
+		# print ddxx
+
+		etheta-=dx/ddxx
+	if etheta>3.00:etheta=3.00
+	if etheta<-3.00:etheta=-3.00
+
+	return etheta
+
+# 
+def esttheta2(a,b,c,u,k=100):
+	global D
+	# print u
+	
+	if u.sum()==u.shape[0]:return 3.00
+	if u.sum()==0:return -3.00
+	res=[]
+	for i in range(k):
+		num_step=0
+		etheta=6*np.random.random_sample()-3
+		dx=1.00
+		eps=1e-6
+		# print 0,etheta,abs(dx),abs(dx)>eps
+		while abs(dx)>eps and num_step<300:
+			# print 'in while'
+			num_step+=1
+			tb=etheta-b
+			p=c+(1-c)/(1+math.e**(-D*a*tb))
+			d=D*a*(u-p)*(p-c)/(1-c)/p
+			dd=D**2*a**2*(p-c)*(u*c-p**2)*(1-p)/(p**2*(1-c)**2)
+
+			dx=d.sum()
+			ddxx=dd.sum()
+			# print ddxx
+			if abs(ddxx)<eps:break
+
+			etheta-=dx/ddxx
+		if etheta>3.00:etheta=3.00
+		if etheta<-3.00:etheta=-3.00
+		lh=likehood(etheta,u,a,b,c)
+		res.append((lh,etheta))
+		# print etheta,lh
+	res.sort()
+	return res[-1][-1]
+
+
+# random choice 
+def makechoice(k):
+	global m,n
+	stu_item={i:np.random.choice(m, k, replace=False) for i in range(n)}
+	item_stu={i:[] for i in range(m)}
+	for stu in stu_item:
+		for item in stu_item[stu]:
+			item_stu[item].append(stu)
+	for item in item_stu:
+		if len(item_stu[item])<1:del item_stu[item]
+	return stu_item,item_stu
+
+# each student select k items randomly,est theta,a,b for each student,select item
+def estall2(k):
+	global a,b,c,y,theta
+	stu_item,item_stu=makechoice(k)
+	etheta=[]
+	# est theta
+	for i in range(len(stu_item)):
+		print 'est theta' ,i
+		etheta.append(esttheta(a[stu_item[i]],b[stu_item[i]],c,y[i,stu_item[i]]))
+
+	
+	for i in range(len(etheta)):
+		if abs(etheta[i])>2.999:
+			for item in item_stu:
+				if i in item_stu[item]:item_stu[item].remove(i)
+	etheta=np.array(etheta)
+	# est a,b
+	eas=[]
+	ebs=[]
+	for item in item_stu:
+		print  'est ab' ,item
+		# print etheta[item_stu[item]]
+		# print y[item_stu[item],item]
+		lh,ea,eb=estab(etheta[item_stu[item]],y[item_stu[item],item],100)	
+		eas.append(ea)
+		ebs.append(eb)
+	eas=np.array(eas)
+	ebs=np.array(ebs)
+	print np.abs(etheta-theta).mean(),np.abs(eas-a).mean(),np.abs(ebs-b).mean()
+	return etheta,eas,ebs
+
+# each student select first k student ,est theta,use the ested theta est a,b for left items
+def estall3(k):
+	global a,b,c,y,theta,n,m
+	etheta=[]
+	# est theta
+	for i in range(n):
+		print 'est theta' ,i
+		etheta.append(esttheta(a[:k],b[:k],c,y[i,:k]))
+	
+	etheta=np.array(etheta)
+	# est a,b
+	eas=[]
+	ebs=[]
+	for i in range(m-k):
+		item=k+i
+		print  'est ab' ,item
+		# print etheta[item_stu[item]]
+		# print y[item_stu[item],item]
+		lh,ea,eb=estab(etheta,y[:,item],100)	
+		eas.append(ea)
+		ebs.append(eb)
+	eas=np.array(eas)
+	ebs=np.array(ebs)
+	print np.abs(etheta-theta).mean(),np.abs(eas-a[k:]).mean(),np.abs(ebs-b[k:]).mean()
+	return etheta,eas,ebs
+
+
+# each student select k items randomly from first m/2 items,est theta,
+# each last m/2 items select k student randomly for est a,b
+def estall4(n_item=100,n_stu=100):
+	global a,b,c,y,theta,n,m
+	etheta=[]
+	# est theta
+	for i in range(n):
+		print 'est theta' ,i
+		indicate=np.random.choice(m/2, n_item, replace=False)
+		etheta.append(esttheta(a[indicate],b[indicate],c,y[i,indicate]))
+	
+	etheta=np.array(etheta)
+	# est a,b
+	eas=[]
+	ebs=[]
+	for i in range(m/2):
+		item=m/2+i
+		print  'est ab' ,item
+		# print etheta[item_stu[item]]
+		# print y[item_stu[item],item]
+		indicate=np.random.choice(n, n_stu, replace=False)
+		lh,ea,eb=estab(etheta[indicate],y[indicate,item],100)	
+		eas.append(ea)
+		ebs.append(eb)
+	eas=np.array(eas)
+	ebs=np.array(ebs)
+	print np.abs(etheta-theta).mean(),np.abs(eas-a[m/2:]).mean(),np.abs(ebs-b[m/2:]).mean()
+	return etheta,eas,ebs
+
+def estalltheta(k):
+	global a,b,c,y,n,m,theta
+	etheta=[]
+	item=np.random.choice(m, k, replace=False)
+	for i in range(n):
+		etheta.append(esttheta2(a[item],b[item],c,y[i,item],200))
+	etheta=np.array(etheta)
+	print etheta
+	print (((etheta - theta)**2).sum()/n)**0.5,np.abs(etheta - theta).mean()
+
+
+	etheta=[]
+	item=np.random.choice(m, k, replace=False)
+	for i in range(n):
+		etheta.append(esttheta(a[item],b[item],c,y[i,item]))
+	etheta=np.array(etheta)
+	print (((etheta - theta)**2).sum()/n)**0.5,np.abs(etheta - theta).mean()
+	return etheta
+
+
 
 
 
